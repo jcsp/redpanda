5c006741b4ab19406f392bd6b89d6e00cdaa0272
John Spray <jcs@vectorized.io>
2021-07-26 14:00:21 +0100

    schema_registry/offset_waiter: carry current offset inside offset waiter
    One less separate thing being managed as a 'only shard zero' member elsewhere.
    
    Enables building in the 'already ready' path to offset_waiter::wait rather than
    relying on caller to make sure they never try and wait on an already-passed offset.
    
    Signed-off-by: John Spray <jcs@vectorized.io>

--- src/v/pandaproxy/schema_registry/offset_waiter.h
+++ src/v/pandaproxy/schema_registry/offset_waiter.h
@@ -21,6 +21,8 @@
 
 namespace pandaproxy::schema_registry {
 
+/// \brief track advancement of a topic offset, and enable waiting
+///        for particular offets to be reached.
 class offset_waiter {
     struct data {
         model::offset _offset;
@@ -32,13 +34,24 @@ public:
       : _waiters{size} {}
 
     ss::future<> wait(model::offset offset) {
+        if (_offset >= offset) {
+            // Already ready!
+            co_return;
+        }
         data d{._offset{offset}};
         auto fut = d._promise.get_future();
         co_await _waiters.push_eventually(std::move(d));
         co_return co_await std::move(fut);
     }
 
+    /// Callers must only call this with monotonically
+    /// increasing offsets increasing by 1 each time,
+    /// apart from their first call which may specify any offset.
     void signal(model::offset offset) {
+        assert(
+          offset == _offset + model::offset{1} || _offset == model::offset{-1});
+        _offset = offset;
+
         while (!_waiters.empty()) {
             if (_waiters.front()._offset <= offset) {
                 _waiters.front()._promise.set_value();
@@ -50,8 +63,11 @@ public:
         }
     }
 
+    model::offset get_offset() { return _offset; }
+
 private:
     ss::queue<data> _waiters;
+    model::offset _offset{-1};
 };
 
 } // namespace pandaproxy::schema_registry
