7da2ed7afefd38ef060647cbc4932abfed9d2dab
John Spray <jcs@redpanda.com>
2022-02-10 18:19:42 +0000

    rpk: implement `cluster config reset`
    This is the offline disaster recovery helper for
    if a configuration value slips through validation
    and puts redpanda nodes into a state where they
    will not start.

---
+++ src/go/rpk/pkg/cli/cmd/cluster/config/reset.go
@@ -0,0 +1,81 @@
+// Copyright 2022 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package config
+
+import (
+	"path/filepath"
+
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/config"
+	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/out"
+	"gopkg.in/yaml.v2"
+)
+
+func newResetCommand(fs afero.Fs) *cobra.Command {
+	var configCacheFile string
+	cmd := &cobra.Command{
+		Use:   "reset [PROPERTY...]",
+		Short: "Forcibly clear a cluster configuration property on this node.",
+		Long: `Forcibly clear a cluster configuration property on this node.
+
+This command is not for general changes to cluster configuration: use this only
+when redpanda will not start due to a configuration issue.
+
+This command erases a named property from the local node's internal 
+cache, so that on next startup redpanda will treat the setting as if it 
+was set to the default.
+`,
+		Run: func(cmd *cobra.Command, propertyNames []string) {
+			p := config.ParamsFromCommand(cmd)
+			cfg, err := p.Load(fs)
+			out.MaybeDie(err, "unable to load config: %v", err)
+
+			dataDir := cfg.Redpanda.Directory
+
+			// Same filename as in redpanda config_manager.cc
+			if configCacheFile == "" {
+				const cacheFileName = "config_cache.yaml"
+				configCacheFile = filepath.Join(dataDir, cacheFileName)
+			}
+
+			// Read YAML
+			f, err := afero.ReadFile(fs, configCacheFile)
+			out.MaybeDie(err, "Couldn't read %q", configCacheFile)
+
+			// Decode YAML
+			content := make(map[string]interface{})
+			err = yaml.Unmarshal(f, content)
+			out.MaybeDie(err, "Couldn't parse %q: %v", configCacheFile, err)
+
+			// Snip out the value we are resetting
+			for _, pn := range propertyNames {
+				delete(content, pn)
+			}
+
+			// Encode output
+			out_bytes, err := yaml.Marshal(content)
+			out.MaybeDie(err, "Serialization error: %v", configCacheFile, err)
+
+			// Write back output
+			err = afero.WriteFile(fs, configCacheFile, out_bytes, 0755)
+			out.MaybeDie(err, "Couldn't write %q: %v", configCacheFile, err)
+		},
+	}
+
+	cmd.Flags().StringVar(
+		&configCacheFile,
+		"cache-file",
+		"",
+		"location of configuration cache file (defaults to redpanda data directory)",
+	)
+
+	return cmd
+}
