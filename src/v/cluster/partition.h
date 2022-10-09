154de252c2b06811ccd49698bf31e21c3bb87360
John Spray <jcs@redpanda.com>
2022-10-09 15:45:59 +0100

    cloud_storage: implement S3 data deletion on topic deletion
    This only happens if remote.write is enabled (either by topic
    override or by global setting), and the topic is not a read
    replica topic.
    
    There are still some edge cases where objects may be left
    behind in S3: this doesn't break anything but it does consume
    some space.  Because manifests and segments are updated non-atomically,
    we will need a 'scrub' type operation to resolve these cases.

--- src/v/cluster/partition.h
+++ src/v/cluster/partition.h
@@ -283,6 +283,7 @@ private:
     ss::shared_ptr<cloud_storage::remote_partition> _cloud_storage_partition;
     ss::lw_shared_ptr<const storage::offset_translator_state> _translator;
     std::optional<s3::bucket_name> _read_replica_bucket{std::nullopt};
+    bool _remote_delete_enabled{topic_properties::default_remote_delete};
 
     friend std::ostream& operator<<(std::ostream& o, const partition& x);
 };
