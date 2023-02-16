581a1345d880bfd661e1c1b103861170b9941e05
John Spray <jcs@redpanda.com>
2023-02-16 16:25:31 +0000

    DNM debug

--- src/v/redpanda/admin_server.cc
+++ src/v/redpanda/admin_server.cc
@@ -626,6 +626,7 @@ ss::httpd::broker_json::maintenance_status fill_maintenance_status(
         ret.eligible = s.eligible.value_or(0);
         ret.failed = s.failed.value_or(0);
     } else {
+        vlog(logger.info, "fill_maintenance_status: no drain status");
         ret.draining = false;
         // ensure that the output json has all fields
         ret.finished = false;
@@ -1982,6 +1983,12 @@ admin_server::get_broker_handler(std::unique_ptr<ss::httpd::request> req) {
           fmt::format(
             "Unexpected error: {}", maybe_drain_status.error().message()),
           ss::httpd::reply::status_type::service_unavailable);
+    }
+    if (maybe_drain_status.value().has_value()) {
+        vlog(logger.info, "get_broker_handler: drain status: {}",
+             maybe_drain_status.value().value());
+    } else {
+        vlog(logger.info, "get_broker_handler: no drain status");
     }
 
     ss::httpd::broker_json::broker ret;
